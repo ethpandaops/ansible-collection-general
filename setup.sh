84151cb6d168afeadd229aaf8a619509edefa1e4
Rafael Matias <rafael@skyle.net>
2022-12-15 12:15:10 +0100

    feat(ci): add sanity tests

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Plugin list
 ASDF_PYAPP_INCLUDE_DEPS=1 asdf plugin add ansible https://github.com/amrox/asdf-pyapp.git
 asdf plugin add ansible-lint https://github.com/amrox/asdf-pyapp.git
+asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git
 
 asdf install
