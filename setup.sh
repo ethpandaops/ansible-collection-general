cf225dba1585680ec6596ab7ef1e544d4daff0e5
Rafael Matias <rafael@skyle.net>
2023-07-04 14:56:46 +0200

    install python tools via pip

--- setup.sh
+++ setup.sh
@@ -4,9 +4,15 @@
 
 ASDF_PYAPP_INCLUDE_DEPS=1 asdf plugin add ansible https://github.com/amrox/asdf-pyapp.git
 asdf plugin add ansible-lint https://github.com/amrox/asdf-pyapp.git
+asdf plugin add molecule https://github.com/amrox/asdf-pyapp.git
+asdf plugin add molecule-containers https://github.com/amrox/asdf-pyapp.git
 asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git
+asdf plugin-add python
 
 asdf install
+
+# Install python tools
+pip install -r requirements.txt
 
 # Install ansible collection depdendencies
 ansible-galaxy collection install -r requirements.yaml --force
