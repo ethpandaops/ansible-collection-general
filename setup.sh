57c2a1212c567d0d191c022f42a7f66602cf9a9d
Rafael Matias <rafael@skyle.net>
2023-07-18 15:39:23 +0200

    remove asdf plugins that moved to pip

--- setup.sh
+++ setup.sh
@@ -2,10 +2,6 @@
 
 # Install required tools using asdf
 
-ASDF_PYAPP_INCLUDE_DEPS=1 asdf plugin add ansible https://github.com/amrox/asdf-pyapp.git
-asdf plugin add ansible-lint https://github.com/amrox/asdf-pyapp.git
-asdf plugin add molecule https://github.com/amrox/asdf-pyapp.git
-asdf plugin add molecule-containers https://github.com/amrox/asdf-pyapp.git
 asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git
 asdf plugin-add python
 
