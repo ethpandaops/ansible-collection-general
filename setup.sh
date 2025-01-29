3603dd82ebcd06761332e7c325f5878c3c2e3dea
Rafael Matias <rafael@skyle.net>
2025-01-29 15:34:15 +0100

    bump pip

--- setup.sh
+++ setup.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 
+set -xe
+
 # Install required tools using asdf
 
-asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git
-asdf plugin-add python
+asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git || true
+asdf plugin-add python || true
 
 asdf install
+asdf reshim
 
 # Install python tools
 pip install -r requirements.txt
